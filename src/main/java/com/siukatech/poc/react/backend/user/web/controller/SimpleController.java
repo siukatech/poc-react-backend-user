ef8b2bde936fe48bc71ae955ac9b96bd90aaa8e3
siukatech <siuka114@gmail.com>
2024-04-11 01:34:02 +0800

    update parent version and add testFixtures

---
+++ src/main/java/com/siukatech/poc/react/backend/user/web/controller/SimpleController.java
@@ -0,0 +1,17 @@
+package com.siukatech.poc.react.backend.user.web.controller;
+
+import com.siukatech.poc.react.backend.parent.web.annotation.base.RestApiController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RestApiController
+public class SimpleController {
+
+    @GetMapping("/v1/public/simple-list")
+    public ResponseEntity<List<String>> listSimple() {
+        return ResponseEntity.ok(List.of("1", "2", "3"));
+    }
+
+}
