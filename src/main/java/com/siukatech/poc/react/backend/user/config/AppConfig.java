870b6bd666d9f5ad3b149b72d64eaddbb4420659
siukatech <siuka114@gmail.com>
2024-10-30 17:56:14 +0800

    feat: change to EnableUserSupport annotation

--- src/main/java/com/siukatech/poc/react/backend/user/config/AppConfig.java
+++ src/main/java/com/siukatech/poc/react/backend/user/config/AppConfig.java
@@ -1,21 +1,23 @@
 package com.siukatech.poc.react.backend.user.config;
 
 import com.siukatech.poc.react.backend.parent.EnableReactBackend;
+import com.siukatech.poc.react.backend.parent.user.EnableUserSupport;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @Configuration
-//@Import({
-//        GlobalConfigImport.class,
-//        WebConfigImport.class
-//        , SecurityConfigImport.class
+////@Import({
+////        GlobalConfigImport.class,
+////        WebConfigImport.class
+////        , SecurityConfigImport.class
+////})
+//@ComponentScan({
+//        "com.siukatech.poc.react.backend.parent.web.controller"
 //})
-@ComponentScan({
-        "com.siukatech.poc.react.backend.parent.web.controller"
-})
-@EnableReactBackend
+//@EnableReactBackend
+@EnableUserSupport
 public class AppConfig {
 
 }
--- src/test/java/com/siukatech/poc/react/backend/user/ReactBackendAppTests.java
+++ src/test/java/com/siukatech/poc/react/backend/user/ReactBackendAppTests.java
@@ -1,18 +1,24 @@
 package com.siukatech.poc.react.backend.user;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
-//@SpringBootTest(properties = {
-//		"client-id=XXX"
-//		, "client-secret=XXX"
-//		, "client-realm=react-backend-realm"
-//		, "spring.profiles.active=dev"
-//		, "oauth2.client.keycloak=http://localhost:38180"
-////		, "oauth2.client.keycloak=XXX"
-//})
-//@OverrideAutoConfiguration(enabled = false)
-//@AutoConfigureDataJpa
+@SpringBootTest
 class ReactBackendAppTests {
+
+	@MockBean
+	private OAuth2ClientProperties oAuth2ClientProperties;
+	@MockBean
+	private InMemoryClientRegistrationRepository clientRegistrationRepository;
+	@MockBean
+	private OAuth2ResourceServerProperties oAuth2ResourceServerProperties;
+	@MockBean
+	private JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
