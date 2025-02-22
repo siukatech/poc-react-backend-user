564bf191f58c88c7b24299f5e78a5af70c780296
siukatech <siuka114@gmail.com>
2025-02-22 18:36:14 +0800

    feat: update caching related and redis client testing

--- src/main/java/com/siukatech/poc/react/backend/user/business/form/SimpleForm.java
+++ src/main/java/com/siukatech/poc/react/backend/user/business/form/SimpleForm.java
@@ -1,12 +1,13 @@
 package com.siukatech.poc.react.backend.user.business.form;
 
+import com.siukatech.poc.react.backend.core.business.form.AbstractForm;
 import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
-public class SimpleForm {
+public class SimpleForm extends AbstractForm<String> {
     private String name;
     private Long count;
     private LocalDate date;
---
+++ src/test/java/com/siukatech/poc/react/backend/user/redis/RedisClientTests.java
@@ -0,0 +1,26 @@
+package com.siukatech.poc.react.backend.user.redis;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class RedisClientTests {
+
+    @Test
+    public void test_redisClient_basic() {
+        String host = "localhost";
+        int port = 36379;
+        String password = "redispass";
+        RedisURI redisURI = RedisURI.builder()
+                .withHost(host)
+                .withPort(port)
+                .withPassword(password.toCharArray())
+                .build();
+        try (RedisClient redisClient = RedisClient.create(redisURI)) {
+            redisClient.connect();
+        }
+    }
+
+}
